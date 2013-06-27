f9999f6cf617457c93201d7a50c104ce1dd4f80b
Niko <nikodemus.schmidt@gmx.net>
2013-06-28 01:50:52 +0200

    rotationen und sonst was korrigiert

--- Tetris-Display/FallingBlocks/reverseZFallingBlock.py
+++ Tetris-Display/FallingBlocks/reverseZFallingBlock.py
@@ -90,77 +90,76 @@ class reverseZFallingBlock(FallingBlock):
                 self.part4.pos = ((self.part4.pos[0] - self.GameMenue.blocksize),self.part4.pos[1] + self.GameMenue.blocksize)
                 self.rotatingPosition = 0
     
-    def hitGround(self): # gibt True zurück, wenn es den "Boden" bzw ein Hindernis berührt, sonst False
+    def hitGround(self): # gibt True zurueck, wenn es den "Boden" bzw ein Hindernis beruehrt, sonst False
         if (self.rotatingPosition == 0): #check part2, 3 und 4
-            a = self.Field.Matrix[self.currPos2[0]][self.currPos2[1] + self.GameMenue.blocksize]
-            b = self.Field.Matrix[self.currPos3[0]][self.currPos3[1] + self.GameMenue.blocksize]
-            c = self.Field.Matrix[self.currPos4[0]][self.currPos4[1] + self.GameMenue.blocksize]
-            if (a[1] > 18) or (a[1] == True) or (b[1] > 18) or (b[1] == True) or (c[1] > 18) or (c[1] == True):
+            a = self.Field.matrix[self.currPos2[0]][self.currPos2[1] + self.GameMenue.blocksize]
+            b = self.Field.matrix[self.currPos3[0]][self.currPos3[1] + self.GameMenue.blocksize]
+            c = self.Field.matrix[self.currPos4[0]][self.currPos4[1] + self.GameMenue.blocksize]
+            if (a[1] > 18) or (a == True) or (b[1] > 18) or (b == True) or (c[1] > 18) or (c == True):
                 return True
             else:
                 return False
         else: # check part1 und part 3
-            a = self.Field.Matrix[self.currPos1[0]][self.currPos1[1] + self.GameMenue.blocksize]
-            b = self.Field.Matrix[self.currPos3[0]][self.currPos3[1] + self.GameMenue.blocksize]
-            if (a[1] > 18) or (a[1] == True) or (b[1] > 18) or (b[1] == True):
+            a = self.Field.matrix[self.currPos1[0]][self.currPos1[1] + self.GameMenue.blocksize]
+            b = self.Field.matrix[self.currPos3[0]][self.currPos3[1] + self.GameMenue.blocksize]
+            if (a[1] > 18) or (a == True) or (b[1] > 18) or (b == True):
                 return True
-            else: return False
+            else: 
+                return False
     
     def checkLeftBound(self): #return true wenn bewegung moeglich
         
-        if (self.rotatingPosition == 0): # prüfe part 1,3
-            a = self.Field.Matrix[self.currPos1[0] - 1][self.currPos1[1]]
-            b = self.Field.Matrix[self.currPos3[0] - 1][self.currPos3[1]]
-            if (b[0] < 0) or (a[0] == True) or (b[0] == True):
+        if (self.rotatingPosition == 0): # pruefe part 1,3
+            a = self.Field.matrix[self.currPos1[0] - 1][self.currPos1[1]]
+            b = self.Field.matrix[self.currPos3[0] - 1][self.currPos3[1]]
+            if (b[0] < 0) or (a == True) or (b == True):
                 return False
             else:
                 return True
-        else: # prüfe part1,2,3
-            a = self.Field.Matrix[self.currPos1[0] - 1][self.currPos1[1]]
-            b = self.Field.Matrix[self.currPos2[0] - 1][self.currPos2[1]]
-            c = self.Field.Matrix[self.currPos3[0] - 1][self.currPos3[1]]
-            if (b[0] < 0) or (a[0] == True) or (b[0] == True) or (c[0] == True):
+        else: # pruefe part1,2,3
+            a = self.Field.matrix[self.currPos1[0] - 1][self.currPos1[1]]
+            b = self.Field.matrix[self.currPos2[0] - 1][self.currPos2[1]]
+            c = self.Field.matrix[self.currPos3[0] - 1][self.currPos3[1]]
+            if (b[0] < 0) or (a == True) or (b == True) or (c == True):
                 return False
             else:
                 return True
     
     def checkRightBound(self):#return true wenn bewegung moeglich
         
-        if (self.rotatingPosition == 0): # prüfe part 2,4
-            a = self.Field.Matrix[self.currPos2[0] + 1][self.currPos2[1]]
-            b = self.Field.Matrix[self.currPos4[0] + 1][self.currPos4[1]]
-            if (a[0] > 13) or (a[0] == True) or (b[0] == True):
+        if (self.rotatingPosition == 0): # pruefe part 2,4
+            a = self.Field.matrix[self.currPos2[0] + 1][self.currPos2[1]]
+            b = self.Field.matrix[self.currPos4[0] + 1][self.currPos4[1]]
+            if (a[0] > 13) or (a == True) or (b == True):
                 return False
             else:
                 return True
-        else: # prüfe part2,3,4
-            a = self.Field.Matrix[self.currPos2[0] + 1][self.currPos2[1]]
-            b = self.Field.Matrix[self.currPos3[0] + 1][self.currPos3[1]]
-            c = self.Field.Matrix[self.currPos4[0] + 1][self.currPos4[1]]
-            if (b[0] > 13) or (a[0] == True) or (b[0] == True) or (c[0] == True):
+        else: # pruefe part2,3,4
+            a = self.Field.matrix[self.currPos2[0] + 1][self.currPos2[1]]
+            b = self.Field.matrix[self.currPos3[0] + 1][self.currPos3[1]]
+            c = self.Field.matrix[self.currPos4[0] + 1][self.currPos4[1]]
+            if (b[0] > 13) or (a == True) or (b == True) or (c == True):
                 return False
             else:
                 return True
     
     def checkCollisionAtRotation(self, RotatingNumber): # True = Collison, False = No Collision
         
-        # muss in beiden Fällen currPos1 und currPos4 abfragen
         if (RotatingNumber == 0):
-            a = self.Field.Matrix[self.currPos2[0] - 2][self.currPos2[1] - 1]
-            b = self.Field.Matrix[self.currPos4[0] - 1][self.currPos1[1] + 1]
-            if (a[1] < 0) or (b[1] > 18):
+            b = self.Field.matrix[self.currPos2[0] - 1][self.currPos2[1] - 1]
+            c = self.Field.matrix[self.currPos3[0] + 2][self.currPos3[1]]
+            d = self.Field.matrix[self.currPos4[0] + 1][self.currPos4[1] - 1]
+            if (b[1] < 0) or (b == True) or (c == True) or (d == True):
                 return True
-            elif (b == True):
-                    return True
             else:
-                    return False
+                return False
                 
         else:
-            a = self.Field.Matrix[self.currPos1[0] - 2][self.currPos1[1] + 1]
-            b = self.Field.Matrix[self.currPos4[0] + 1][self.currPos1[1] - 1]
-            if (a[0] < 0) or (b[0] > 18):
+            b = self.Field.matrix[self.currPos2[0] + 1][self.currPos2[1] + 1]
+            c = self.Field.matrix[self.currPos3[0] - 2][self.currPos3[1]]
+            d = self.Field.matrix[self.currPos4[0] - 1][self.currPos4[1] + 1]
+            if (c[0] < 0) or (b == True) or (c == True) or (d == True):
                 return True
-            elif (b == True) or (a == True):
-                    return True
             else:
-                    return False
+                return False
+            
